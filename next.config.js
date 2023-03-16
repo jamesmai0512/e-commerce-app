50e708153b1f270a8b92a025bd28a197c146041b
Khai Mai <khai.mai@asnet.com.vn>
2023-03-16 14:25:43 +0700

    fix: update the resolve the fs

--- next.config.js
+++ next.config.js
@@ -15,6 +15,17 @@ const nextConfig = {
     locales: ['en'],
     defaultLocale: 'en',
   },
+
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
