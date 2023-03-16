e8a673dc71d94dd5293f7e8350f2d29a6c128204
Khai Mai <khai.mai@asnet.com.vn>
2023-03-16 15:30:48 +0700

    feat: Update the custom 404 page

--- src/pages/__test__/404.test.tsx
+++ src/pages/__test__/404.test.tsx
@@ -14,19 +14,4 @@ describe('Custom404', () => {
     const button = component.getByText('Return To Home')
     expect(button).toBeInTheDocument()
   })
-
-  // it('Should navigate to home page on button click', () => {
-  //   const mockPush = jest.fn()
-
-  //   const history = createMemoryHistory({ initialEntries: ['/asdf'] });
-  //   const component = render(
-  //     <Router history={history}>
-  //       <Custom404 />
-  //     </Router>,
-  //   )
-  //   const button = component.getByText('Return To Home')
-
-  //   fireEvent.click(button)
-  //   expect(mockPush).toHaveBeenCalledWith('/')
-  // })
 })
